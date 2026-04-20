baa60e8a67ad479cf1f14427425c409bb3cdc977
DeviJv <septian.ardi.wijaya@gmail.com>
2026-04-20 21:34:50 +0700

    coba

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,11 +2,11 @@
 
 # Automated database sync on startup
 echo "Syncing database with Prisma schema..."
-npx prisma db push
+npx prisma db push --schema=app/generated/prisma/schema.prisma
 
 if [ $? -eq 0 ]; then
   echo "Database is in sync. Seeding data..."
-  npx prisma db seed
+  npx prisma db seed --schema=app/generated/prisma/schema.prisma
   echo "Seeding completed. Starting the application..."
 else
   echo "Database sync failed. Attempting to start the application anyway..."
